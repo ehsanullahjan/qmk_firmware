f66ab3b5ee89b5bf0b06790fbc56c319a3a2b96a
ehsanj <ehsanullahjan@gmail.com>
2021-07-06 16:49:24 -0400

    Enable Space Cadet Shift and fix repeating keys on FN layer

--- keyboards/kinesis/keymaps/ehsanj/keymap.c
+++ keyboards/kinesis/keymaps/ehsanj/keymap.c
@@ -6,7 +6,7 @@
 #define COLEMAK     0
 #define QWERTY      1
 #define FLAYOUT     2
-#define FKEYS      3
+#define FKEYS       3
 #define NKEYS       4
 #define MOUSE       5
 #define NUMPAD      6
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TG(FLAYOUT),        KC_1,                   KC_2,                   KC_3,               KC_4,                   KC_5,
         KC_EQL,             KC_Q,                   KC_W,                   KC_F,               KC_P,                   KC_G,
         KC_GRV,             KC_A,                   KC_R,                   KC_S,               KC_T,                   KC_D,
-        KC_LSFT,            KC_Z,                   KC_X,                   KC_C,               KC_V,                   KC_B,
+        LSFT_T(KC_LBRC),    KC_Z,                   KC_X,                   KC_C,               KC_V,                   KC_B,
                             KC_CAPS,                LCTL_T(KC_BSLS),        LALT_T(KC_LEFT),    LGUI_T(KC_RGHT),
         // left thumb
                                                     LGUI_T(KC_DEL),         LALT_T(KC_F11),
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_6,               KC_7,                   KC_8,                   KC_9,               KC_0,                   TG(MOUSE),
         KC_J,               KC_L,                   KC_U,                   KC_Y,               KC_SCLN,                KC_MINS,
         KC_H,               KC_N,                   KC_E,                   KC_I,               KC_O,                   KC_QUOT,
-        KC_K,               KC_M,                   KC_COMM,                KC_DOT,             KC_SLSH,                KC_RSFT,
+        KC_K,               KC_M,                   KC_COMM,                KC_DOT,             KC_SLSH,                RSFT_T(KC_RBRC),
                             RGUI_T(KC_UP),          RALT_T(KC_DOWN),        RCTL_T(KC_LBRC),    KC_RBRC,
         // right thumb
                                                     RALT_T(KC_F12),         RGUI_T(KC_ESC),
@@ -201,11 +201,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,                _______,                _______,            _______,                _______,        _______,    _______,    _______,
         _______,            _______,                _______,                _______,            _______,                _______,
         _______,            _______,                _______,                _______,            _______,                _______,
-        KC_GRV,             _______,                _______,                _______,            _______,                _______,
         _______,            _______,                _______,                _______,            _______,                _______,
+        KC_LSFT,            _______,                _______,                _______,            _______,                _______,
                             _______,                KC_BSLS,                KC_LEFT,            KC_RGHT,
         // left thumb
-                                                    KC_RGUI,                KC_LALT,
+                                                    KC_DEL,                 KC_LALT,
                                                                             KC_LCTL,
                             KC_BSPC,                KC_TAB,                 _______,
 
@@ -214,10 +214,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,            _______,                _______,                _______,            _______,                _______,
         _______,            _______,                _______,                _______,            _______,                _______,
         _______,            _______,                _______,                _______,            _______,                _______,
-        _______,            _______,                _______,                _______,            _______,                _______,
+        _______,            _______,                _______,                _______,            _______,                KC_RSFT,
                             KC_UP,                  KC_DOWN,                KC_LBRC,            _______,
         // right thumb
-                                                    KC_RALT,                KC_LGUI,
+                                                    KC_RALT,                KC_RGUI,
                                                     KC_RCTL,
                                                     _______,                KC_ENT,             KC_SPC
     ),
